68c2d71abe5078e3f3ad80c1f9963f5d6c2d32ab
Un4ckn0wl3z <gh0stsync.rt@gmail.com>
2018-04-19 06:39:49 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,2 @@
-Abraham Lincoln — 'Give me six hours to chop down a tree and I will spend the first four sharpening the axe.'
+Abraham Lincoln — 'Give me six hours to chop down a tree and I will spend the first four sharpening the axe.' echo '<br>';
+Steve Jobs — 'Stay hungry, stay foolish.' echo '<br>';
